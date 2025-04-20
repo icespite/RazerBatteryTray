18128a9832958898a3b854390351a2524323e090
IceSpite <tracyspite@gmail.com>
2025-04-20 14:46:13 +0800

    feat: add status refresh

--- razer_battery_tray.py
+++ razer_battery_tray.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python3
-
 import os
 import sys
+from functools import lru_cache
 from time import sleep
-import pystray
-from pystray import Icon, Menu, MenuItem
+
 import openrazer.client
 import PIL.Image
-from functools import lru_cache
+from pystray import Icon, Menu, MenuItem
 
 RESIZE_TO_PIXELS = 32
 SCALE = (RESIZE_TO_PIXELS, RESIZE_TO_PIXELS)
@@ -15,7 +13,7 @@ UPDATE_INTERVAL_IN_SECS = 1 / 5
 
 print("Starting Razer Battery Tray")
 
-real_path = os.path.realpath(__file__) # Finding target of Symlink
+real_path = os.path.realpath(__file__)  # Finding target of Symlink
 script_path = os.path.dirname(real_path)
 icons_path = os.path.join(script_path, "icons")
 
@@ -29,11 +27,12 @@ try:
     print("Openrazer version: ", openrazer.client.__version__)
     print("Python version: ", sys.version)
     print("Path of the icons:", icons_path)
-    
+
     try:
         if sys.version_info < (3, 8):
             raise Exception("Python 3.8 or higher is required")
         from importlib.metadata import version
+
         print("Pystray version: ", version("pystray"))
     except:
         print("Cannot get pystray version (Python 3.8+ is required for that..):")
@@ -42,29 +41,34 @@ except:
 
 
 a = openrazer.client.DeviceManager()
+device = None
+print(a.devices)
 if len(sys.argv) < 2:
-    print("\n!! OOPS: Missing device name as argument !!\n")
-    print("Usage: razer_bat_tray.py <part_of_the_device_name>")
-    print("Example 1: razer_bat_tray.py 'Razer Viper Ultimate'")
-    print("Example 2: razer_bat_tray.py 'DeathAdder'\n")
-
-    print("Listing Available Devices:")
     for dev in a.devices:  # print all bat devices
         print(dev.name, "\n    Has Battery:", dev.has("battery"))
-    sys.exit(1)
-
-device_name_param = sys.argv[1].lower()
-device = None
-
-for dev in a.devices:  # find the device we want to monitor
-    if device_name_param in dev.name.lower():
         device = dev
-        print("Found device:", dev.name)
-        break
+else:
+    device_name_param = sys.argv[1].lower()
+
+    for dev in a.devices:  # find the device we want to monitor
+        if device_name_param in dev.name.lower():
+            device = dev
+            print("Found device:", dev.name)
+            break
 
 if device is None:
     print("Device not found, check the name and try again.")
     sys.exit(1)
+
+
+def refrash():
+    print("Refrashing")
+    a = openrazer.client.DeviceManager()
+    global device
+    for dev in a.devices:  # print all bat devices
+        print(dev.name, "\n    Has Battery:", dev.has("battery"))
+        device = dev
+    on_monitor(tray_icon)
 
 
 @lru_cache(maxsize=256)
@@ -95,19 +99,43 @@ def setup_icon(icon):
 
     # start the update loop
     while True:
-        bat_level = device.battery_level
-        is_charging = device.is_charging
-        cur_icon = get_icon(bat_level, is_charging)
-        if cur_icon != icon.icon:
-            icon.icon = cur_icon
-            print(f"Icon set to {bat_level}% {'(charging)' if is_charging else ''}")
-        sleep(UPDATE_INTERVAL_IN_SECS)
+        try:
+            bat_level = device.battery_level
+            is_charging = device.is_charging
+            cur_icon = get_icon(bat_level, is_charging)
+            if cur_icon != icon.icon:
+                icon.icon = cur_icon
+                print(f"Icon set to {bat_level}% {'(charging)' if is_charging else ''}")
+            sleep(UPDATE_INTERVAL_IN_SECS)
+        except KeyboardInterrupt:
+            print("Exiting")
+            break
+        except Exception as e:
+            print("Error:", e)
+            sleep(UPDATE_INTERVAL_IN_SECS)
+            refrash()
 
 
+def on_monitor(icon):
+    global device, device_name
+    device_name = device.name
+
+
+device_name = device.name
 tray_icon = Icon(
     "BatteryIcon",
     get_icon(100),
-    menu=Menu(MenuItem(f'Exit Battery Viewer of "{device.name}"', lambda: os._exit(0))),
+    menu=Menu(
+        MenuItem(
+            lambda text: device_name,
+            on_monitor,
+        ),
+        MenuItem(f"refrash", lambda: refrash()),
+        MenuItem(
+            f"Exit",
+            lambda: os._exit(0),
+        ),
+    ),
 )
 
 tray_icon.run(setup=setup_icon)
